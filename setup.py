d50d417460d3754273d1ddc30a58a3bb693907af
Adones Cunha <adonescunha@gmail.com>
2011-09-14 15:57:09 -0300

    Fix license information.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: MIT License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
